e35da9f1a473a3cd1711430e768f048cb18f5e81
giraffu <cv1347968277@gmail.com>
2026-05-07 16:39:27 +0800

    文件清理

--- test_api.py
+++
@@ -1,3 +0,0 @@
-import requests
-import json
-print("Let's just inspect the code again, we already know the answer")
--- test_pydantic.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-class A(BaseModel):
-    status: str
-
-a = A(status="done", task_type="video")
-print(a.dict())
